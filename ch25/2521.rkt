913b59e400b1f7bb79d52e624b40f1c003f69a30
zhyucan <zhyucan@gmail.com>
2017-11-26 16:33:56 +0800

    ch25

---
+++ ch25/2521.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |2521|) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
+(list 11 9 2 18 12 14 4 1)
+
+(list 9 2 4 1)
+11
+(list 18 12 14)
+
+(list 2 4 1)
+9
+11
+(list 12 14)
+18
+
+(list 1)
+2
+(list 4)
+9
+11
+(list 12 14)
+18
+
+(list 1 2 4 9 12 14 18)
